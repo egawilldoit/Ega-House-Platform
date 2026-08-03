e91107bb46c0ce24bf5c42538993794027958223
MORTAKI0 <115146963+MORTAKI0@users.noreply.github.com>
2026-08-03 14:54:26 +0100

    feat: compose operational studies homepage

---
+++ src/app/home/home-page.tsx
@@ -0,0 +1,31 @@
+import "./home.css";
+
+import { HomeMotion } from "./components/home-motion";
+import { StudyHeader } from "./components/study-header";
+import { ConversionStudy } from "./sections/conversion-study";
+import { FocusStudy } from "./sections/focus-study";
+import { GoalsStudy } from "./sections/goals-study";
+import { HeroStudy } from "./sections/hero-study";
+import { PlanningStudy } from "./sections/planning-study";
+import { ReviewStudy } from "./sections/review-study";
+
+export function HomePage() {
+  return (
+    <HomeMotion>
+      <div className="home-page">
+        <a className="home-skip-link" href="#intro">
+          Skip to homepage content
+        </a>
+        <StudyHeader />
+        <main className="home-page__main">
+          <HeroStudy />
+          <GoalsStudy />
+          <PlanningStudy />
+          <FocusStudy />
+          <ReviewStudy />
+          <ConversionStudy />
+        </main>
+      </div>
+    </HomeMotion>
+  );
+}
